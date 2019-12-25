7fcacef13e9d37f373d4e366c3191aa8730b5c0d
Arsen Utepbergenov <arsen.utepbergenov@altenar.com>
2019-12-25 16:36:07 +0300

    master/updated packages

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,18 @@
-<template>
+<template functional>
   <div class="menu">
-    <router-link class="menu__link" :to="item.nav" v-for="(item, index) in menuItems" :key="index">
+    <router-link class="menu__link" :to="item.nav" v-for="(item, index) in props.menuItems" :key="index">
       {{ item.title }}
     </router-link>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { createComponent } from '@vue/composition-api';
 
-@Component
-export default class Menu extends Vue {
-  @Prop() private menuItems!: object[];
-}
+const Menu = createComponent({
+  props: {
+    menuItems: Array,
+  },
+});
+export default Menu;
 </script>
